2371d9485df3e35c7af11e4853fcc02c5ad3824d
SOURAV GARAI <101336956+Garaisourav12@users.noreply.github.com>
2024-02-17 23:58:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,53 @@
+
+// class Person {
+// 	constructor(name,age) {
+// 		this.name = name;
+// 		this.age = age;
+// 	}
+// 	greet(){
+// 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old. `)
+// 	}
+// }
+// class Employee extends Person{
+// 	constructor(name,age,jobTitle) {
+// 		super();
+// 		this.name = name;
+// 		this.age = age;
+// 		this.jobTitle = jobTitle;
+		
+// 	}
+// 	jobGreet(){
+// 		    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+// 	}
+// }
+// // const alice = new Person("Alice",25);
+// alice.greet();
+// const bob = new Employee("Bob","30","Manager");
+// bob.greet();
+// bob.jobGreet();
+// // Do not change code below this line
+// window.Person = Person;
+// window.Employee = Employee;
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
-
-Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+Person.prototype.greet = function() { //method using prototype
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
+
 
 function Employee(name, age, jobTitle) {
   Person.call(this, name, age);
   this.jobTitle = jobTitle;
 }
 
-Employee.prototype = Object.create(Person.prototype);
-Employee.prototype.constructor = Employee;
+Employee.prototype = Object.create(Person.prototype);// prototype of person is inherited into employee
+Employee.prototype.constructor = Employee; //get constructor of employee
 
 Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
-
-const person = new Person("Alice", 25);
-const employee = new Employee("Alice", 25, "Manager");
-
-person.greet();
-employee.greet();
-employee.jobGreet();
 
 // Do not change code below this line
 window.Person = Person;
